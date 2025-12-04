470a2915696b4c4b206609d3e73e932baee998eb
Vincenzo888111 <kornvathanak@gmail.com>
2025-12-04 13:38:39 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/',  // Make sure this is set
+  base: 'https://github.com/Vincenzo888111/shop',  // Make sure this is set
 });
